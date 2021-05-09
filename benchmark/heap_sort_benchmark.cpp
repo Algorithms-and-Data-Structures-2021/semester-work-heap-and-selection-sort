496c6ae258ee62ddd2849a1561f8a8cf47e276d8
Semen Sokolov <simionsimion@yandex.ru>
2021-05-09 13:57:18 +0300

    min update

--- benchmark/heap_sort_benchmark.cpp
+++ benchmark/heap_sort_benchmark.cpp
@@ -1,3 +1,24 @@
 //
 // Created by Семен Соколов on 08.05.2021.
 //
+#include <fstream>
+#include <iostream>
+#include <string>       // string
+#include <string_view>  // string_view
+#include <chrono>       // high_resolution_clock, duration_cast, nanoseconds
+#include <vector>
+#include <sstream>
+#include "heap_sort.hpp"
+
+// подключаем вашу структуру данных
+
+using namespace std;
+//using namespace itis;
+
+// абсолютный путь до набора данных и папки проекта
+static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR_HEAP_SORT};
+static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
+
+int main() {
+
+}
